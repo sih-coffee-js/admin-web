e6efe19dcc56caf37a902f1ddde58851545fae89
Vishesh1110 <visheshvividh@gmail.com>
2024-09-05 15:27:51 +0530

    Updated dashboard after login

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -2,18 +2,11 @@ import { useState} from "react";
 import SideBarLayout from "./SideBarLayout";
 
 const Dashboard = () => {
-  const [currentView, setCurrentView] = useState("home");
-
+  const [currentView, setCurrentView] = useState("users");
 
   return (
     <SideBarLayout currentView={currentView} setCurrentView={setCurrentView}>
-      {currentView === "home" && (
-        <>
-          <div className="flex-grow p-8 text-gray-100">
-            <h1 className="text-4xl font-bold mb-4">Welcome to the Dashboard</h1>
-          </div>          
-        </>
-      )}
+      {currentView === "users" }
     </SideBarLayout>
   );
 };
